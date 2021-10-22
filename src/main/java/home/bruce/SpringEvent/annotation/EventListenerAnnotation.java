b7f6edbd7e6b7151f2c2f758609afab36751697b
bruce <bruce>
2021-10-22 10:35:37 +0800

    增加另一種用法

--- src/main/java/home/bruce/SpringEvent/annotation/EventListenerAnnotation.java
+++ src/main/java/home/bruce/SpringEvent/annotation/EventListenerAnnotation.java
@@ -1,7 +1,6 @@
 package home.bruce.SpringEvent.annotation;
 
 import home.bruce.SpringEvent.event.MyEvent;
-import home.bruce.SpringEvent.event.MyEvent2;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -13,8 +12,13 @@ public class EventListenerAnnotation {
         System.out.println("annotation1=" + applicationEvent);
     }
 
-    @EventListener({MyEvent.class, MyEvent2.class}) // 可用來取代 ApplicationListener<XxxEvent>，也可兩者都用
+    //    @EventListener({MyEvent.class, MyEvent2.class}) // 可用來取代 ApplicationListener<XxxEvent>，也可兩者都用
     public void ooo(ApplicationEvent applicationEvent) {
         System.out.println("annotation2=" + applicationEvent);
+    }
+
+    @EventListener
+    public void qoo(MyEvent myEvent) { // 這種寫法只能一個參數
+        System.out.println("@EventListener.myEvent=" + myEvent);
     }
 }
